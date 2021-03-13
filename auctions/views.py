fd81350c5d526096d4d9add1d75da30013f73a44
John Gutierrez <djterez88@gmail.com>
2021-03-12 19:19:16 -0800

    Queried info in index.html

--- auctions/views.py
+++ auctions/views.py
@@ -4,11 +4,14 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import *
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+    entry = AuctionListing.objects.all()
+
+    context = {'entry': entry}
+    return render(request, "auctions/index.html", context)
 
 
 def login_view(request):
